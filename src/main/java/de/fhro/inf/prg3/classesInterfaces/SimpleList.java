f4fac96bc2bd3ca63ba9a8ac840f7679fefeb7c5
daschub <daniel.schubert@stud.fh-rosenhheim.de>
2017-11-14 15:25:46 +0100

    first task solved

--- src/main/java/de/fhro/inf/prg3/classesInterfaces/SimpleList.java
+++ src/main/java/de/fhro/inf/prg3/classesInterfaces/SimpleList.java
@@ -13,9 +13,4 @@ public class SimpleList {
 
     }
 
-    public class SimpleIterator{
-
-
-    }
-
 }
--- src/main/java/de/fhro/inf/prg3/classesInterfaces/SimpleListImpl.java
+++ src/main/java/de/fhro/inf/prg3/classesInterfaces/SimpleListImpl.java
@@ -8,23 +8,70 @@ public class SimpleListImpl implements SimpleList, Iterable<Object> {
     private Element head;
     private int size;
 
+    public SimpleListImpl(){
+        this.head = null;
+    }
 
-    void add (Object item){
-        return;
+    /**
+     * add Object to the end of the list
+     * @param item
+     */
+    public void add (Object item){
+
+        if(head == null){
+            head = new Element(item);
+        }
+        else{
+            Element current = head;
+            while(current.getNext() != null)
+                current = current.getNext();
+
+            current.setNext(new Element(item));
+        }
+        size++;
     }
 
     public static class Element {
-        int next;
+        public Element next;
         Object item;
+
+        Element(Object item) {
+            this.next = null;
+            this.item = item;
+        }
+
+        public Element getNext() {
+            return next;
+        }
+
+        public Object getItem() {
+            return item;
+        }
+
+        public void setNext(Element next) {
+            this.next = next;
+        }
     }
-    
 
     @Override
-    public java.util.Iterator<Object> iterator() {
-        return null;
+    public Iterator<Object> iterator() {
+        return new SimpleIterator();
     }
 
+    private class SimpleIterator implements Iterator{
 
+        private Element current = head;
 
+        @Override
+        public boolean hasNext() {
+            return current != null;
+        }
 
+        @Override
+        public Object next() {
+            Object tmp = current.getItem();
+            current = current.getNext();
+            return tmp;
+        }
+    }
 }
