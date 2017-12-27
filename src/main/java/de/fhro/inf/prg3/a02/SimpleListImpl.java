a864eb7271519c0a4e4791d2949776dc6852f482
daschub <daniel.schubert@stud.fh-rosenhheim.de>
2017-12-27 23:43:18 +0100

    added main methods without logic

--- src/main/java/de/fhro/inf/prg3/a02/SimpleListImpl.java
+++ src/main/java/de/fhro/inf/prg3/a02/SimpleListImpl.java
@@ -1,10 +1,20 @@
 package de.fhro.inf.prg3.a02;
 
+import java.util.Iterator;
+
 /**
  * @author Peter Kurfer
  * Created on 10/6/17.
  */
-public class SimpleListImpl implements SimpleList {
+public class SimpleListImpl implements SimpleList, Iterable<Object> {
+
+    private Element head;
+    private int size;
+
+    public SimpleListImpl() {
+        this.head = null;
+    }
+
     @Override
     public void add(Object o) {
 
@@ -12,7 +22,7 @@ public class SimpleListImpl implements SimpleList {
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
@@ -21,6 +31,49 @@ public class SimpleListImpl implements SimpleList {
     }
 
     // TODO: Implement the required methods.
+
+
+    @Override
+    public Iterator iterator() {
+        return new SimpleIteratorImpl();
+    }
+
+    private static class Element{
+
+        private Object item;
+        private Element next;
+        public Element(Object item){
+            this.next = null;
+            this.item = item;
+        }
+
+        public Object getItem() {
+            return item;
+        }
+
+        public Element getNext() {
+            return next;
+        }
+
+        public void setNext(Element next) {
+            this.next = next;
+        }
+    }
+
+    private class SimpleIteratorImpl implements Iterator<Object>{
+
+        private Element current = head;
+
+        @Override
+        public boolean hasNext() {
+            return false;
+        }
+
+        @Override
+        public Object next() {
+            return null;
+        }
+    }
 
 
 }
